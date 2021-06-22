497bcfbf3d25e9b349bc26adfc2de7fe41679455
Student05 <student05@TFNS5305.local>
2021-06-22 11:03:18 +0200

    first commit

---
+++ MarquetPierre_iOS_Eva2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarquetPierre_iOS_Eva2
+//
+//  Created by Student05 on 22/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MarquetPierre_iOS_Eva2Tests/MarquetPierre_iOS_Eva2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  MarquetPierre_iOS_Eva2Tests.swift
+//  MarquetPierre_iOS_Eva2Tests
+//
+//  Created by Student05 on 22/06/2021.
+//
+
+import XCTest
+@testable import MarquetPierre_iOS_Eva2
+
+class MarquetPierre_iOS_Eva2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
